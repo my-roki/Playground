f353dead4e07e2a32be4c7fea3fb55590e492f78
my-roki <anthonychangrok@gmail.com>
2022-05-13 17:59:36 +0900

    Django Doc : Writing your first Django app, part 4

--- python/learn_Django/learn_django/polls/urls.py
+++ python/learn_Django/learn_django/polls/urls.py
@@ -8,3 +8,11 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+# # Use generic views: Less code is better, but not junior
+# urlpatterns = [
+#     path("", views.IndexView.as_view(), name="index"),
+#     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+#     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
