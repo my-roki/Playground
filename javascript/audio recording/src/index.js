97b66351454b5e303ee7bec382f0a526f234c66e
my-roki <anthonychangrok@gmail.com>
2022-07-29 05:15:39 +0900

    audio recorder

--- javascript/audio recording/src/index.js
+++ javascript/audio recording/src/index.js
@@ -3,27 +3,57 @@ import "regenerator-runtime";
 const startButton = document.getElementById("start-button");
 const audio = document.getElementById("audio");
 
+let stream;
+let recorder;
+let audioFile;
+let recorderTimeout = null;
+
 const init = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false,
   });
+  audio.srcObject = stream;
 };
 
 const startRecording = () => {
-  //   audio.srcObject = stream;
-  //   audio.autoplay = true;
-
   startButton.innerText = "Stop recording";
   startButton.removeEventListener("click", startRecording);
   startButton.addEventListener("click", stopRecording);
+
+  recorder = new MediaRecorder(stream);
+  console.log(recorder);
+  recorder.ondataavailable = (event) => {
+    audioFile = URL.createObjectURL(event.data);
+    audio.srcObject = null;
+    audio.src = audioFile;
+    audio.play();
+  };
+  recorder.start();
+  recorderTimeout = setTimeout(() => {
+    stopRecording();
+  }, 5000);
 };
 
 const stopRecording = () => {
-  startButton.innerText = "Start recording";
+  if (recorderTimeout) {
+    clearTimeout(recorderTimeout);
+    recorderTimeout = null;
+  }
+  startButton.innerText = "Download recording";
   startButton.removeEventListener("click", stopRecording);
-  startButton.addEventListener("click", startRecording);
+  startButton.addEventListener("click", downRecording);
+
+  recorder.stop();
 };
+
+function downRecording() {
+  const a = document.createElement("a");
+  a.href = audioFile;
+  a.download = "my_recording.webm";
+  document.body.append(a);
+  a.click();
+}
 
 init();
 startButton.addEventListener("click", startRecording);
