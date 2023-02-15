434a9fcf10406c7aa9d575d39b2e1e494d4bd850
my-roki <anthonychangrok@gmail.com>
2023-02-15 14:52:13 +0900

    Session Login build project

---
+++ Java/session-login/src/test/java/com/myroki/sessionLogin/SessionLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myroki.sessionLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
