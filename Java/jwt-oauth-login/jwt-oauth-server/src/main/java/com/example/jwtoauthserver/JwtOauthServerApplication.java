2d8df6bc1a28bcda7e927a35c09410bd31c88cce
my-roki <anthonychangrok@gmail.com>
2023-02-22 14:33:08 +0900

    oauth2를 이용한 jwt 로그인 빌드

---
+++ Java/jwt-oauth-login/jwt-oauth-server/src/main/java/com/example/jwtoauthserver/JwtOauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOauthServerApplication.class, args);
+	}
+
+}
---
+++ Java/jwt-oauth-login/jwt-oauth-server/src/test/java/com/example/jwtoauthserver/JwtOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
